4aae537dbc38d0cbff1cffb5eb1e84a0168b5020
rasmuschr9 <rasmus@rkc-dev.dk>
2017-03-24 08:47:23 +0100

    first commit

---
+++ Aarhus_Web_Developer_Coop/Controllers/ContactFormSurfaceController.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Web.Mvc;
+using Aarhus_Web_Developer_Coop.ViewModels;
+using System.Net.Mail;
+using Umbraco.Core.Models;
+
+namespace AarhusWebDevCoop.Controllers
+{
+    public class ContactFormSurfaceController : SurfaceController
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return PartialView("ContactForm", new ContactForm());
+        }
+        [HttpPost]
+        public ActionResult HandleFormSubmit(ContactForm model)
+        {
+            MailMessage message = new MailMessage();
+
+            if (!ModelState.IsValid) { return CurrentUmbracoPage(); }
+                message.To.Add("rasmus.k.christiansen@gmail.com");
+                message.Subject = model.Subject;
+                message.From = new MailAddress(model.Email, model.Name);
+                message.Body = model.Message;
+
+            IContent comment = Services.ContentService.CreateContent(model.Subject, CurrentPage.Id, "message");
+            comment.SetValue("messageName", model.Name);
+            comment.SetValue("email", model.Email);
+            comment.SetValue("subject", model.Subject);
+            comment.SetValue("messageContent", model.Message);
+
+            //Save
+            Services.ContentService.Save(comment);
+
+            //Save and publish
+            //Services.ContentService.SaveAndPublishWithStatus(comment);
+
+
+            using (SmtpClient smtp = new SmtpClient())
+                {
+                    smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtp.UseDefaultCredentials = false;
+                    smtp.EnableSsl = true;
+                    smtp.Host = "smtp.gmail.com";
+                    smtp.Port = 587;
+                    smtp.Credentials = new System.Net.NetworkCredential("rasmus.k.christiansen@gmail.com", "redshadow");
+                    smtp.EnableSsl = true;
+                    // send mail
+                    smtp.Send(message);
+                }
+            TempData["success"] = true;
+            return RedirectToCurrentUmbracoPage();  
+        }
+    }
+}
---
+++ Aarhus_Web_Developer_Coop/ViewModels/ContactForm.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace Aarhus_Web_Developer_Coop.ViewModels
+{
+    public class ContactForm
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Required]
+        [EmailAddress(ErrorMessage = "This is not a valid email address")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Subject { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
